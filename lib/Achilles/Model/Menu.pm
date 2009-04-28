78d9ea36e1e7d90b08217ad0a68dc93a1d4c8e62
Devin Austin <devin@devin-laptop.(none)>
2009-04-27 20:41:43 -0600

    updated per hobbs' changes

--- lib/Achilles/Model/Menu.pm
+++ lib/Achilles/Model/Menu.pm
@@ -6,17 +6,21 @@ use base qw/Catalyst::Model::MenuGrinder/;
 
 __PACKAGE__->config(
   menu_config => {
-    plugins => [
-      'XMLLoader',
-      'FileReloader',
-      'DefaultTarget',
-      'Variables',
-      'ActivePath',
-      'NullOutput',
-    ],
+    plugins => {
+        loader => 'XMLLoader',
+        on_load => [
+            'DefaultTarget',
+        ],
+        per_request => [
+            'FileReloader',
+            'Variables',
+            'ActivePath',
+        ],
+    },
     filename => Achilles->path_to('root', 'menus', 'top-nav.xml'),
   },
 );
+
 
 1;
 
